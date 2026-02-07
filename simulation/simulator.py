c2ad6ff1dd8f63f25bce39feb61ebe91c832ed5d
Joshi Shreyas <248840231+Shreyas2608@users.noreply.github.com>
2026-02-07 15:21:38 +0530

    Add basic order simulation

--- simulation/simulator.py
+++ simulation/simulator.py
@@ -1,1 +1,34 @@
+import random
+from utils.logger import log_info
+from utils.time_utils import current_timestamp
+
+def generate_random_order():
+    order_type = random.choice(["BUY", "SELL"])
+    order_kind = random.choice(["LIMIT", "MARKET"])
+    quantity = random.randint(1, 20)
+
+    order = {
+        "type": order_type,
+        "kind": order_kind,
+        "quantity": quantity,
+        "timestamp": current_timestamp()
+    }
+
+    if order_kind == "LIMIT":
+        order["price"] = random.randint(90, 110)
+
+    return order
+
+def run_simulation(n=10):
+    log_info("Simulation started")
+
+    for _ in range(n):
+        order = generate_random_order()
+        log_info(f"Generated Order: {order}")
+        print(order)
+
+    log_info("Simulation finished")
+
+if __name__ == "__main__":
+    run_simulation()
 
